628fef0e3e0d08f4d8aff7105c8e6021be0b702b
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-02-21 13:18:32 -0300

    footer 2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <footer>
-        <small>
+      <footer style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <span>
           Copyright &copy;{" "}
           {this.props.sharedBasicInfo
             ? this.props.sharedBasicInfo.name
             : "???"}
-        </small>
+        </span>
       </footer>
     );
   }
