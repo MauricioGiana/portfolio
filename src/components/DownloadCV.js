d13c2b9562b7f9ad24753912511feb89638e70c2
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-02-23 21:25:22 -0300

    toggle

--- src/components/DownloadCV.js
+++ src/components/DownloadCV.js
@@ -1,60 +1,59 @@
 import React, { Component } from "react";
+import $ from "jquery";
 
 class DownloadCV extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            show: false
-        };
-        this.toggleShow = this.toggleShow.bind(this);
-    }
 
-    toggleShow = () => {
-        this.setState({
-            show: !this.state.show
+    componentDidMount() {
+        $("#cvs-box").hide();
+
+        $(".slide-cvs").click(function () {
+            $("#cvs-box").slideToggle(500);
         });
-    };
+    }
 
     render() {
         return (
             <div id="download-cv">
-                <div className="download-btn" onClick={this.toggleShow}>{this.props.title}</div>
-                {this.state.show && (
-                    <div id="cvs-box">
-                        <a
-                            href="https://drive.google.com/file/d/1xC__BR-q2ExZLVHHUwicO2uWKkFjGxyN/view?usp=sharing"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            onClick={this.toggleShow}
-                        >
-                            <span
-                                className="iconify cv-lang-icon"
-                                data-icon="twemoji-flag-for-flag-united-kingdom"
-                                data-inline="false"
-                                id={window.$primaryLanguageIconId}
-                            ></span>
-                            English
-                        </a>
-                        <a
-                            href="https://drive.google.com/file/d/1m1M8fJFRYGKOehk7bV3zT5_-0kYrB8EE/view?usp=sharing"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            onClick={this.toggleShow}
-                        >
-                            <span
-                                className="iconify cv-lang-icon"
-                                data-icon="twemoji-flag-for-flag-argentina"
-                                data-inline="false"
-                                id={window.$primaryLanguageIconId}
-                            ></span>
-                            Español
-                        </a>
-                    </div>
-                )
-                }
+                <div className="download-btn slide-cvs" onClick={() => toggleShow()}>{this.props.title}</div>
+                <div id="cvs-box">
+                    <a
+                        href="https://drive.google.com/file/d/1xC__BR-q2ExZLVHHUwicO2uWKkFjGxyN/view?usp=sharing"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => toggleShow()}
+                        className="slide-cvs"
+                    >
+                        <span
+                            className="iconify cv-lang-icon"
+                            data-icon="twemoji-flag-for-flag-united-kingdom"
+                            data-inline="false"
+                            id={window.$primaryLanguageIconId}
+                        ></span>
+                        English
+                    </a>
+                    <a
+                        href="https://drive.google.com/file/d/1m1M8fJFRYGKOehk7bV3zT5_-0kYrB8EE/view?usp=sharing"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        onClick={() => toggleShow()}
+                        className="slide-cvs"
+                    >
+                        <span
+                            className="iconify cv-lang-icon"
+                            data-icon="twemoji-flag-for-flag-argentina"
+                            data-inline="false"
+                            id={window.$primaryLanguageIconId}
+                        ></span>
+                        Español
+                    </a>
+                </div>
             </div>
         );
     }
+}
+
+function toggleShow() {
+
 }
 
 export default DownloadCV;
