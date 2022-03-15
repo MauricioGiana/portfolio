ca773187f4fee453500a92b6814166e4c8dae071
Mauricio Giana <jmauricio@hotmail.com.ar>
2022-03-15 15:11:29 -0300

    skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -6,20 +6,22 @@ class Skills extends Component {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
       var skills = this.props.sharedSkills.icons.map(function (skills, i) {
         return (
-          <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-          </li>
+          <a href={skills.link} target="_blank" rel="noopener noreferrer" className="skill-link">
+            <li className="list-inline-item mx-3" key={i}>
+              <span>
+                <div className="text-center skills-tile">
+                  <i className={skills.class} style={{ fontSize: "220%" }}>
+                    <p
+                      className="text-center"
+                      style={{ fontSize: "30%", marginTop: "4px" }}
+                    >
+                      {skills.name}
+                    </p>
+                  </i>
+                </div>
+              </span>
+            </li>
+          </a>
         );
       });
     }
