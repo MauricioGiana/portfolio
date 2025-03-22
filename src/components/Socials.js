15f9d5e481a6aa2ed0be6f433c6ad5885f453e55
Mauricio Giana <mgiana@revaicare.com>
2025-03-22 17:59:53 -0300

    add pointer and title to socials

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Socials extends Component {
-    render() {
-        return (
-            <div className='socials-container'>
-                <ul>
-                    {
-                        this.props.data?.map(item => (
-                            <li key={item.name}>
-                                <a href={item.url} target="_blank" rel="noopener noreferrer">
-                                    <i className={item.class}></i>
-                                </a>
-                            </li>
-                        ))
-                    }
-                </ul>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="socials-container">
+        <ul>
+          {this.props.data?.map((item) => (
+            <li key={item.name}>
+              <a
+                href={item.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                style={{ cursor: "pointer" }}
+                title={item.url}
+              >
+                <i className={item.class}></i>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default Socials;
