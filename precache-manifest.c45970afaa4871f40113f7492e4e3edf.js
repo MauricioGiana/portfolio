9c625795f1c8718b70a218b0d007f7cf3b26ddd2
Mauricio Giana <mgiana@revaicare.com>
2025-03-23 13:19:12 -0300

    Updates

---
+++ precache-manifest.c45970afaa4871f40113f7492e4e3edf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d261f9fbe1d2fdc57cd78a1f99b9eec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf5ebbd2abd08aa972bd",
+    "url": "/static/css/2.bf458638.chunk.css"
+  },
+  {
+    "revision": "377e3014fa3cb3cc46a4",
+    "url": "/static/css/main.3ec88465.chunk.css"
+  },
+  {
+    "revision": "cf5ebbd2abd08aa972bd",
+    "url": "/static/js/2.6e5790e1.chunk.js"
+  },
+  {
+    "revision": "b5ce6286fe99b565568bc15caa58d380",
+    "url": "/static/js/2.6e5790e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "377e3014fa3cb3cc46a4",
+    "url": "/static/js/main.10ef4f0d.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  },
+  {
+    "revision": "47054038c35be1c5f897cd826372fdc0",
+    "url": "/static/media/aws_api_gateway.47054038.svg"
+  },
+  {
+    "revision": "823cb8d6f0346c8d005c842bcc61e526",
+    "url": "/static/media/aws_lambda.823cb8d6.svg"
+  },
+  {
+    "revision": "15d54d142da2f2d6f2e90ed1d55121af",
+    "url": "/static/media/fa-brands-400.15d54d14.ttf"
+  },
+  {
+    "revision": "cbcf42b2e9228a8f5bad42717d8a88db",
+    "url": "/static/media/fa-brands-400.cbcf42b2.woff2"
+  },
+  {
+    "revision": "262525e2081311609d1fdab966c82bfc",
+    "url": "/static/media/fa-regular-400.262525e2.ttf"
+  },
+  {
+    "revision": "89672701a5874b80be27649e0494e354",
+    "url": "/static/media/fa-regular-400.89672701.woff2"
+  },
+  {
+    "revision": "269f971cec0d5dc864fe9ae080b19e23",
+    "url": "/static/media/fa-solid-900.269f971c.ttf"
+  },
+  {
+    "revision": "4a6591ab5460ae5cbff1ecbd6e52193a",
+    "url": "/static/media/fa-solid-900.4a6591ab.woff2"
+  },
+  {
+    "revision": "4ed293ceaca9b5b2d9cd74a477963fae",
+    "url": "/static/media/fa-v4compatibility.4ed293ce.ttf"
+  },
+  {
+    "revision": "a772da7bff216cd36214bc44165bba3e",
+    "url": "/static/media/fa-v4compatibility.a772da7b.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.8cbd1e794741f963e8f5fe197b5ea726.js"
+  "/precache-manifest.c45970afaa4871f40113f7492e4e3edf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
