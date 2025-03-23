8f1337803447f0f6f8d0ef3c397078117d34044f
Mauricio Giana <mgiana@revaicare.com>
2025-03-23 13:33:26 -0300

    Updates

---
+++ precache-manifest.6435c0a0360bef2e5dd35be4b6e3225b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfebc5235c139e9e7edfe32f7edae194",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6f5add55a60aca5bc00c",
+    "url": "/portfolio/static/css/2.c9111f33.chunk.css"
+  },
+  {
+    "revision": "93cf834f35af49c12b0b",
+    "url": "/portfolio/static/css/main.a6a70955.chunk.css"
+  },
+  {
+    "revision": "6f5add55a60aca5bc00c",
+    "url": "/portfolio/static/js/2.6e5790e1.chunk.js"
+  },
+  {
+    "revision": "b5ce6286fe99b565568bc15caa58d380",
+    "url": "/portfolio/static/js/2.6e5790e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93cf834f35af49c12b0b",
+    "url": "/portfolio/static/js/main.449df07f.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "15d54d142da2f2d6f2e90ed1d55121af",
+    "url": "/portfolio/static/media/fa-brands-400.15d54d14.ttf"
+  },
+  {
+    "revision": "cbcf42b2e9228a8f5bad42717d8a88db",
+    "url": "/portfolio/static/media/fa-brands-400.cbcf42b2.woff2"
+  },
+  {
+    "revision": "262525e2081311609d1fdab966c82bfc",
+    "url": "/portfolio/static/media/fa-regular-400.262525e2.ttf"
+  },
+  {
+    "revision": "89672701a5874b80be27649e0494e354",
+    "url": "/portfolio/static/media/fa-regular-400.89672701.woff2"
+  },
+  {
+    "revision": "269f971cec0d5dc864fe9ae080b19e23",
+    "url": "/portfolio/static/media/fa-solid-900.269f971c.ttf"
+  },
+  {
+    "revision": "4a6591ab5460ae5cbff1ecbd6e52193a",
+    "url": "/portfolio/static/media/fa-solid-900.4a6591ab.woff2"
+  },
+  {
+    "revision": "4ed293ceaca9b5b2d9cd74a477963fae",
+    "url": "/portfolio/static/media/fa-v4compatibility.4ed293ce.ttf"
+  },
+  {
+    "revision": "a772da7bff216cd36214bc44165bba3e",
+    "url": "/portfolio/static/media/fa-v4compatibility.a772da7b.woff2"
+  }
+]);
